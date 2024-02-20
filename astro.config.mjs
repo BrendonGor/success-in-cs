aa4fa63ba13a9b15c9d05bf0f01c7bbe104fa376
BrendonGor <brendon.t.gorelik@gmail.com>
2024-02-20 12:08:39 -0700

    Switched home logo and links

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'CS 1960',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/BrendonGor/success-in-cs',
 			},
 			sidebar: [
 				{
