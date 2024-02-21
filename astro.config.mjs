e5db2bc61cc4a66db1332b4fda73abfad4b8dc93
BrendonGor <brendon.t.gorelik@gmail.com>
2024-02-20 22:47:43 -0700

    prepared for github pages hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	// without this pages config don't need base
+	site: 'https://brendongor.github.io/success-in-cs/',
+	base: '/success-in-cs', // seems starlight already prepends this to the links in sidebar but not to rest internal page links
 	integrations: [
 		starlight({
 			title: 'CS 1960',
